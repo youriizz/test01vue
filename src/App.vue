5f2788939aa790edca301258802f5ca3820de0e0
Youri Zermatten <yourizermatten@gmail.com>
2024-05-09 11:16:30 +0200

    commit tuto 4

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <AppModal />
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AppModal from './components/AppModal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { AppModal },
+  data() {
+    return {
+      title : 'my first vue app :)'
+    }
+  }, 
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+    }
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
